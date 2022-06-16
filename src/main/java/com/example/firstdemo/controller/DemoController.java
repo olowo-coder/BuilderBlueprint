f4c88aced2ffb8bc3904197621208732b2806a9c
olowo-coder <lanre75olowo@gmail.com>
2022-06-16 21:06:20 +0100

    Builder style

--- src/main/java/com/example/firstdemo/controller/DemoController.java
+++ src/main/java/com/example/firstdemo/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.example.firstdemo.controller;
 
+import com.example.firstdemo.dto.UserDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -12,6 +15,16 @@ public class DemoController {
 
     @GetMapping("/app/test")
     public ResponseEntity<?> getResponse(){
-        return ResponseEntity.ok(Map.of("message", "yes"));
+
+        return ResponseEntity.ok(Map.of("message", new UserDto.Builder()
+                .firstName("Ben").lastName("Mike").age(40).address("silver street")
+                .build()));
+    }
+
+    @PostMapping("/app/generate")
+    public ResponseEntity<?> deserialize(@RequestBody final UserDto userDto){
+        System.out.println(userDto);
+        userDto.setAge(900);
+        return ResponseEntity.ok(Map.of("generated", userDto));
     }
 }
--- src/main/java/com/example/firstdemo/dto/UserDto.java
+++ src/main/java/com/example/firstdemo/dto/UserDto.java
@@ -1,2 +1,117 @@
-package com.example.firstdemo.dto;public class UserDto {
+package com.example.firstdemo.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.google.gson.GsonBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    //All final attributes
+    private  String firstName; // required
+    private  String lastName; // required
+    private  int age; // optional
+    private  String phone; // optional
+    private  String address; // optional
+
+    public UserDto(String firstName, String lastName, int age, String phone, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    private UserDto(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.phone = builder.phone;
+        this.address = builder.address;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    //All getter, and NO setter to provde immutability
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(this);
+    }
+
+    public static class Builder
+    {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String phone;
+        private String address;
+
+        public Builder() {
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+        public UserDto build() {
+            UserDto user =  new UserDto(this);
+            validateUserObject(user);
+            return user;
+        }
+        private void validateUserObject(UserDto user) {
+            //Do some basic validations to check
+            //if user object does not break any assumption of system
+        }
+    }
 }
